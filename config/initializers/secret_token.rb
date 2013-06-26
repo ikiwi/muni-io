86d1f42deea9f281a65755dfe8238545cce36820
Greg Cooper <me@gre.gs>
2013-06-25 22:07:08 -0700

    Postgres

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MuniIo::Application.config.secret_token = 'bc2b92c16c9e3931338a80b026b7a1505ffd18c56bcf56eb71f2e9c0175998ba2ba09113131166b908d64927f9cf71e8e8469786d76b28f75eae2573dc0e9f7c'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MuniIo::Application.config.secret_key_base = '1a67c82aa7e7c7518eb91aef05b936fa13f71e15822bf67e3cb1293621adaf97ca6237e75768bff9b89ceccd88f445d7bf55e4b991d0d1dc5ca1cd430999c693'
