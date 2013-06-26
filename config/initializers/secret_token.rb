8bfcf1828884568f8db34aa9c74c177e96a67608
Greg Cooper <me@gre.gs>
2013-06-25 19:59:36 -0700

    Changed to a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuniIo::Application.config.secret_token = 'bc2b92c16c9e3931338a80b026b7a1505ffd18c56bcf56eb71f2e9c0175998ba2ba09113131166b908d64927f9cf71e8e8469786d76b28f75eae2573dc0e9f7c'
